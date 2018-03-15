e50845f0508d678a90b5f81669e4d5e2b238977a
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-14 20:19:41 -0400

    deploytogoogle

--- insertRecords.js
+++ insertRecords.js
@@ -1,5 +1,18 @@
 function insertRecord(type,keyval)
 {
-    var prop = PropertiesService.getScriptProperties().getProperty(type);
-return [type,keyval];
+    try{
+        var prop = PropertiesService.getScriptProperties().getProperty(type);
+        if(prop){
+            var doc = SpreadsheetApp.openById(prop);
+            var sheet = doc.getSheetByName("Sheet1");
+            var lastRow = sheet.getLastRow(),lastCol=sheet.getLastColumn();
+
+            var cols = sheet.getRange(1,1,1,lastCol).values();
+            return [type,keyval,cols];
+        }
+        return [type,keyval];        
+    }catch(e){
+        return [e, type,keyval];
+    }
+
 }
