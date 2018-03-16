25fb8f57c34b4ff9d4f3b94dd2c7b173c9709f21
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-16 15:26:40 -0400

    deploytogoogle

--- insertRecords.js
+++ insertRecords.js
@@ -2,20 +2,9 @@ function insertRecord(type,keyval)
 {
     try{
         var et = new entity(type);
+        var exists = et.findMatchByCol("UPC",keyval['UPC']);
         
-        if(prop){
-            var kv = JSON.parse(keyval);
-            var doc = SpreadsheetApp.openById(prop);
-            var sheet = doc.getSheetByName("Sheet1");
-            var lastRow = sheet.getLastRow(),lastCol=sheet.getLastColumn();
-
-            var cols = sheet.getRange(1,1,1,lastCol).getValues()[0];
-            var id = sheet.getRange(2,1,lastRow,1).getValues();
-            var ids = [].concat.apply([], id);
-            var rec = recordsExists(type,kv.UPC);
-            return JSON.stringify([type,kv,cols,Math.max.apply(null,ids)+1,[lastRow,lastCol]],rec);
-        }
-        return JSON.stringify(["no prop",type,keyval]);        
+        return exists;
     }catch(e){
         return JSON.stringify(["error", e.message,type,keyval]);
     }
