4cd2b2b16374a52cf175a7c673622b492b7e19e9
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-19 12:00:27 -0400

    deploytogoogle

--- insertRecords.js
+++ insertRecords.js
@@ -3,7 +3,7 @@ function insertRecord(obj)
     try{
         var type = obj.type;
         var et = new entity(type);
-        return et.insert(JSON_PARSE(obj[1]));
+        return et.insert(obj.record);
     }catch(e){
         return JSON.stringify(["error", e.message,obj]);
     }
