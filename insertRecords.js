045b6edc499f89c4a50f7a8a7ec5c495c002dc53
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-19 16:50:01 -0400

    deploytogoogle

--- insertRecords.js
+++ insertRecords.js
@@ -8,26 +8,3 @@ function insertRecord(obj)
         return JSON.stringify(["error", e.message,obj]);
     }
 }
-
-function recordsExists(type,upc)
-{
-    try{
-        var prop = PropertiesService.getScriptProperties().getProperty(type);
-        var keys = PropertiesService.getScriptProperties().getProperty('KEYFIELDS');
-        var attributes = getAttributes(type);
-        var colKey = attributes.indexOf("UPC");
-        var doc = SpreadsheetApp.openById(prop);
-        var sheet = doc.getSheetByName("Sheet1");
-        var lastRow = sheet.getLastRow(),lastCol=sheet.getLastColumn();
-        var lookup = sheet.getRange(2,colKey,lastRow,1).getValues().findIndex(upc);
-        if(lookup != -1){
-            return sheet.getRange(lookup+1,1,1,lastCol).getValues();
-        }else{
-            return false;
-        }        
-     }
-     catch(e)
-     {
-        return JSON.stringify(["error", e.message,type,keyval]);
-    }
-}
