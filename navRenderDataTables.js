8ac7227ecc89d5b6be104a74dcb84e0949ca64c4
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-20 13:30:08 -0400

    deploytogoogle

--- navRenderDataTables.js
+++ navRenderDataTables.js
@@ -1,7 +1,5 @@
 function renderDataTable(type)
 {
     var et = new entity(type);
-    return ContentService
-          .createTextOutput(JSON.stringify({tbl:type,records:et.records}))
-          .setMimeType(ContentService.MimeType.JSON);
+    return [type,et.records];
 }
