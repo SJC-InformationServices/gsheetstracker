f64146585ec87a9e2709bf3f10120f826baa9e69
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-21 16:24:12 -0400

    deploytogoogle

--- authorized.js
+++ authorized.js
@@ -12,15 +12,15 @@ function authorize(){
     var email = Session.getActiveUser().getEmail();
     if(email.indexOf("@stjoseph.com") !== -1)
     {
-        var appDriveFolderId = SCRIPT_PROP.getProperty('DRIVEFOLDERID');
-        var F = DriveApp.getFolderById(appDriveFolderId);
-        var permission = F.getAccess(Session.getActiveUser().getEmail());
+        //var appDriveFolderId = SCRIPT_PROP.getProperty('DRIVEFOLDERID');
+        //var F = DriveApp.getFolderById(appDriveFolderId);
+        //var permission = F.getAccess(Session.getActiveUser().getEmail());
         return true;
-        if(permission == DriveApp.Permission.EDIT){
+        /*if(permission == DriveApp.Permission.EDIT){
         return true;
         }else{
             return false;
-        }
+        }*/
     }
     else {
         return false;
