933ff89c66fcb7a110b0ebbf4bbc3e13d879ab75
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-21 14:18:37 -0400

    deploytogoogle

--- navRenderDataTables.js
+++ navRenderDataTables.js
@@ -4,6 +4,11 @@ function renderDataTable(type)
     var et = new entity(type);
     return {type:type,rec:JSON.parse(JSON.stringify(et.records)),attributes:JSON.parse(JSON.stringify(et.attributes))};
     }catch(e){
-        return {"error":e.getMessage(),type:type,rec:JSON.parse(JSON.stringify([])),attributes:JSON.parse(JSON.stringify([]]))};
+        return {
+            "error":e.getMessage(),
+            type:type,
+            rec:[],
+            attributes:[]
+        };
     }
 }
