0a47d13f56d6469ccb02805966ca76f53786e646
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-22 14:01:04 -0400

    deploytogoogle

--- _init.js
+++ _init.js
@@ -4,7 +4,11 @@ var lock = LockService.getPublicLock();
 lock.waitLock(30000);  // wait 30 seconds before conceding defeat.
 var authorization =  authorize();
 
-var SJCARCHIVE=Object.create(null,{MODELS:{value:{}}});
+var SJCARCHIVE=Object.create(null,{
+  MODELS:{
+    value:{}
+  }
+});
 
 function doGet(e)
 {
