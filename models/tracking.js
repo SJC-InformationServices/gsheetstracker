cbb811625a996b92522d162f5102c24491a0af1c
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-22 12:11:51 -0400

    deploytogoogle

--- models/tracking.js
+++ models/tracking.js
@@ -1,5 +1,5 @@
 
-SJCARCHIVE.MODELS.TRACKING= TRACKING = {
+SJCARCHIVE.MODELS.TRACKING = TRACKING = {
     type:"TRACKING",
     docKey: PropertiesService.getScriptProperties().getProperty(this.type),
     doc:null,
