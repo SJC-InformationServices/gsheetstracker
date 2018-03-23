64e5cf4de19aa15c8708dcc05172ed9a36988466
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-23 15:09:27 -0400

    deploytogoogle

--- appScripts/sjcArchive.js
+++ appScripts/sjcArchive.js
@@ -71,3 +71,47 @@ var sjcArchiveOutput = Object.create(null, {
   });
 return sjcArchiveOutput;
 }
+
+function getDataTableAttributes(type)
+{
+    try{
+    var et = new entity(type);
+    return {type:type,rec:JSON.parse(JSON.stringify(et.records)),attributes:JSON.parse(JSON.stringify(et.attributes))};
+    }catch(e){
+        return {err:e,type:type,rec:JSON.parse(JSON.stringify([])),attributes:JSON.parse(JSON.stringify([]))};
+    }
+}
+
+function insertRecord(obj)
+{
+    try
+    {  
+        var archiveOutPut = new modules_sjcarchive();              
+        return JSON.stringify(archiveOutPut.createModel(obj));
+        //continue here
+    }catch(e){
+        return JSON.stringify(["error", e.message,obj,this.sjcArchiveOutput]);
+    }
+}
+
+function updateRecord(type,keyval,row)
+{    
+return [type,keyval];
+/*try{
+    var prop = PropertiesService.getScriptProperties().getProperty(type);
+    if(prop)
+    {
+        var doc = SpreadsheetApp.openById(prop);
+        var sheet = doc.getSheetByName("Sheet1");
+        var lastRow = sheet.getLastRow(),lastCol=sheet.getLastColumn();
+            
+        var cols = sheet.getRange(2,1,lastRow-1,lastCol);
+        var values = cols.getValues();
+        return values;
+    }else{
+        return false;
+    }}
+    catch(e){
+        return false;
+    }*/
+}
