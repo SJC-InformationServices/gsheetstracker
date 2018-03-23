731a4fb59a588200625474f9e4c7111a0b2565ee
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-23 14:56:56 -0400

    deploytogoogle

--- doGetRequest.js
+++ doGetRequest.js
@@ -1,82 +1,81 @@
-var sjcArchiveOutput = Object.create(null, {
-  parameters: {
-    value: {},
-  },
-  models:{
-    value:{
-      TRACKING:model_tracking(),
-      PRODUCTS:model_products(),
-      OFFERS:model_offers(),
-      IMAGES:model_images()      
-    },
-  },
-  modules:{
-    value:{
-    }
-  },
-  authorize: {
-    get: function () {
-      var email = Session.getActiveUser().getEmail();
-      if (email.indexOf("@stjoseph.com") !== -1) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  },
-  perms: {
-    get: function () {
-      //TODO: dbl check permissions on gsheets
-      return true;
-    }
-  },
-  unAuthorizedTemplate: {
-    value: 'html/unAuthorized'
-  },
-  AuthorizedTemplate: {
-    value: 'html/index.html'
-  },
-  template: {
-    get: function () {
-      if (this.authorize) {
-        return this.AuthorizedTemplate;
-      } else {
-        return this.unAuthorizedTemplate;
-      }
-    }
-  },
-  render: {
-    value: function () {
-      var template = HtmlService.createTemplateFromFile(this.template);
-      results = template.evaluate();
-      results.setFaviconUrl("https://s3.amazonaws.com/sjcarchiveassets/lib/images/favicon.ico");
-      results.setTitle('SJC McKesson Archive Manager');
-      results.addMetaTag('viewport', 'width=device-width, initial-scale=1');
-      return results;
-    }
-  },
-  createModel:{
-    value:function(type,keyval){
-      var model = Object.create(this.models[type],keyval);
-      return model;
-    }
-  },
-  updateModel:{
-    value:function(type,id,keyval){}
-  },
-  deleteModel:{
-    value: function(type,id){}
-  }
-});
-
 function doGet(e) 
 {   
   var lock = LockService.getPublicLock();
   lock.waitLock(30000);  // wait 30 seconds before conceding defeat.
   try{
-    var archiveOutPut = Object.create(sjcArchiveOutput);
-    archiveOutPut.parameters=e.parameter;
-   return archiveOutPut.render();   
+    var sjcArchiveOutput = Object.create(null, {
+      parameters: {
+        value: {},
+      },
+      models:{
+        value:{
+          TRACKING:model_tracking(),
+          PRODUCTS:model_products(),
+          OFFERS:model_offers(),
+          IMAGES:model_images()      
+        },
+      },
+      modules:{
+        value:{
+        }
+      },
+      authorize: {
+        get: function () {
+          var email = Session.getActiveUser().getEmail();
+          if (email.indexOf("@stjoseph.com") !== -1) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+      perms: {
+        get: function () {
+          //TODO: dbl check permissions on gsheets
+          return true;
+        }
+      },
+      unAuthorizedTemplate: {
+        value: 'html/unAuthorized'
+      },
+      AuthorizedTemplate: {
+        value: 'html/index.html'
+      },
+      template: {
+        get: function () {
+          if (this.authorize) {
+            return this.AuthorizedTemplate;
+          } else {
+            return this.unAuthorizedTemplate;
+          }
+        }
+      },
+      render: {
+        value: function () {
+          var template = HtmlService.createTemplateFromFile(this.template);
+          results = template.evaluate();
+          results.setFaviconUrl("https://s3.amazonaws.com/sjcarchiveassets/lib/images/favicon.ico");
+          results.setTitle('SJC McKesson Archive Manager');
+          results.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+          return results;
+        }
+      },
+      createModel:{
+        value:function(type,keyval){
+          var model = Object.create(this.models[type],keyval);
+          return model;
+        }
+      },
+      updateModel:{
+        value:function(type,id,keyval){}
+      },
+      deleteModel:{
+        value: function(type,id){}
+      }
+    });
+    
+    sjcArchiveOutput.parameters=e.parameter;
+   return sjcArchiveOutput.render();   
   }
   catch(e){
     // if error return this
