01715b14a246fa4ca52c7162dac57739a12b7d08
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-29 12:07:02 -0400

    deploytogoogle

--- appScripts/models/images.js
+++ appScripts/models/images.js
@@ -1,4 +1,20 @@
 function model_images(){
     model_base.call(this,"IMAGES");
+    this.columnDefition = [              
+        {"data":"ID","defaultContent": "","editable":false,"type":"int"},
+        {"data":"UPC","defaultContent": "","type":"string"},
+        {"data":"TRACKED_UPC","defaultContent": "","editable":false,"type":"string"},
+        {"data":"DESCRIPTION","defaultContent": "","type":"string"},
+        {"data":"IN_SJC","defaultContent": "","type":"datetime"},
+        {"data":"IN_SJC_COUNT","defaultContent": "","editable":false},
+        {"data":"OUT_SJC","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"OUT_SJC_CONTENT","defaultContent": "","editable":false},
+        {"data":"PHOTODONE","defaultContent": "","type":"datetime"},
+        {"data":"PHOTO_DONE_COUNT","defaultContent": "","searchable": false},
+        {"data":"RECORD_COMPLETE","defaultContent": "","type":"datetime"},
+        {"data":"LAST_USER","defaultContent": "","searchable": false},
+        {"data":"CREATEDON","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"UPDATEDON","defaultContent": "","editable":false,"type":"datetime"}
+      ];
 }
 model_images.prototype = Object.create(model_base.prototype);
--- appScripts/models/offers.js
+++ appScripts/models/offers.js
@@ -1,5 +1,21 @@
 function model_offers(){
     model_base.call(this,"OFFERS");
+    this.columnDefition = [              
+        {"data":"ID","defaultContent": "","editable":false,"type":"int"},
+        {"data":"UPC","defaultContent": "","type":"string"},
+        {"data":"TRACKED_UPC","defaultContent": "","editable":false,"type":"string"},
+        {"data":"DESCRIPTION","defaultContent": "","type":"string"},
+        {"data":"IN_SJC","defaultContent": "","type":"datetime"},
+        {"data":"IN_SJC_COUNT","defaultContent": "","editable":false},
+        {"data":"OUT_SJC","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"OUT_SJC_CONTENT","defaultContent": "","editable":false},
+        {"data":"PHOTODONE","defaultContent": "","type":"datetime"},
+        {"data":"PHOTO_DONE_COUNT","defaultContent": "","searchable": false},
+        {"data":"RECORD_COMPLETE","defaultContent": "","type":"datetime"},
+        {"data":"LAST_USER","defaultContent": "","searchable": false},
+        {"data":"CREATEDON","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"UPDATEDON","defaultContent": "","editable":false,"type":"datetime"}
+      ];
 }
 model_offers.prototype = Object.create(model_base.prototype);
 if(typeof this.MODELS == "undefined"){
