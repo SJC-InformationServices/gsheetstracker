67f542b67356fa51a6586c655db9d910b7d1c30a
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-29 11:09:47 -0400

    deploytogoogle

--- appScripts/models/tracking.js
+++ appScripts/models/tracking.js
@@ -3,20 +3,20 @@ function model_tracking(keyval) {
 
     model_base.call(this,"TRACKING");
     this.columnDefition = [              
-        {"data":"ID","defaultContent": ""},
-        {"data":"UPC","defaultContent": ""},
-        {"data":"TRACKED_UPC","defaultContent": ""},
-        {"data":"DESCRIPTION","defaultContent": ""},
-        {"data":"IN_SJC","defaultContent": ""},
-        {"data":"IN_SJC_COUNT","defaultContent": "","searchable": false},
-        {"data":"OUT_SJC","defaultContent": "","visible":false,"searchable": false},
-        {"data":"OUT_SJC_CONTENT","defaultContent": "","searchable": false},
-        {"data":"PHOTODONE","defaultContent": "","visible":false,"searchable": false},
+        {"data":"ID","defaultContent": "","editable":false,"type":"int"},
+        {"data":"UPC","defaultContent": "","type":"string"},
+        {"data":"TRACKED_UPC","defaultContent": "","editable":false,"type":"string"},
+        {"data":"DESCRIPTION","defaultContent": "","type":"string"},
+        {"data":"IN_SJC","defaultContent": "","type":"datetime"},
+        {"data":"IN_SJC_COUNT","defaultContent": "","editable":false},
+        {"data":"OUT_SJC","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"OUT_SJC_CONTENT","defaultContent": "","editable":false},
+        {"data":"PHOTODONE","defaultContent": "","type":"datetime"},
         {"data":"PHOTO_DONE_COUNT","defaultContent": "","searchable": false},
-        {"data":"RECORD_COMPLETE","defaultContent": "","visible":false,"searchable": false},
+        {"data":"RECORD_COMPLETE","defaultContent": "","type":"datetime"},
         {"data":"LAST_USER","defaultContent": "","searchable": false},
-        {"data":"CREATEDON","defaultContent": "","visible":false,"searchable": false},
-        {"data":"UPDATEDON","defaultContent": "","searchable": false}
+        {"data":"CREATEDON","defaultContent": "","editable":false,"type":"datetime"},
+        {"data":"UPDATEDON","defaultContent": "","editable":false,"type":"datetime"}
       ];
     this.ID = null;
     this.UPC = null;
