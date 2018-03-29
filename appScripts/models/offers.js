5cc51a57425365f14c5f58d6a95b5e91cdd66ceb
kevin.noseworthy@stjoseph.com <kevin.noseworthy@stjoseph.com>
2018-03-29 13:46:51 -0400

    deploytogoogle

--- appScripts/models/offers.js
+++ appScripts/models/offers.js
@@ -1,21 +1,126 @@
 function model_offers(){
     model_base.call(this,"OFFERS");
-    this.columnDefition = [              
-        {"data":"ID","defaultContent": "","editable":false,"type":"int"},
-        {"data":"UPC","defaultContent": "","type":"string"},
-        {"data":"TRACKED_UPC","defaultContent": "","editable":false,"type":"string"},
-        {"data":"DESCRIPTION","defaultContent": "","type":"string"},
-        {"data":"IN_SJC","defaultContent": "","type":"datetime"},
-        {"data":"IN_SJC_COUNT","defaultContent": "","editable":false},
-        {"data":"OUT_SJC","defaultContent": "","editable":false,"type":"datetime"},
-        {"data":"OUT_SJC_CONTENT","defaultContent": "","editable":false},
-        {"data":"PHOTODONE","defaultContent": "","type":"datetime"},
-        {"data":"PHOTO_DONE_COUNT","defaultContent": "","searchable": false},
-        {"data":"RECORD_COMPLETE","defaultContent": "","type":"datetime"},
-        {"data":"LAST_USER","defaultContent": "","searchable": false},
-        {"data":"CREATEDON","defaultContent": "","editable":false,"type":"datetime"},
-        {"data":"UPDATEDON","defaultContent": "","editable":false,"type":"datetime"}
-      ];
+    this.tableId = "#OFFERSTABLE";
+    this.dataTable = {
+        dom: "<'row'<'col-sm-3'B><'col-md-5'p><'col-sm-3'l>><'row'<'col-md-6'i><'col-md-4'f>><'row'<'col-md-12't>>",
+        data: this.records(),
+        paging: true,
+        processing: true,
+        autowidth: true,
+        deferRender: true,
+        keys: true,
+        select: {
+            style: 'single',
+            items: 'row',
+            blurable: false
+        },
+        idSrc: "ID",
+        buttons: [
+            "excel",
+            {
+                extend: 'create',
+                editor: editor
+            },
+            {
+                extend: 'edit',
+                editor: editor
+            },
+            {
+                extend: 'remove',
+                editor: editor
+            }
+        ],
+        columns : [{
+                "data": "ID",
+                "defaultContent": ""
+            },
+            {
+                "data": "UPC",
+                "defaultContent": ""
+            },
+            {
+                "data": "TRACKED_UPC",
+                "defaultContent": ""
+            },
+            {
+                "data": "DESCRIPTION",
+                "defaultContent": ""
+            },
+            {
+                "data": "IN_SJC",
+                "defaultContent": ""
+            },
+            {
+                "data": "IN_SJC_COUNT",
+                "defaultContent": ""
+            },
+            {
+                "data": "OUT_SJC",
+                "defaultContent": ""
+            },
+            {
+                "data": "OUT_SJC_CONTENT",
+                "defaultContent": ""
+            },
+            {
+                "data": "PHOTODONE",
+                "defaultContent": ""
+            },
+            {
+                "data": "PHOTO_DONE_COUNT",
+                "defaultContent": ""
+            },
+            {
+                "data": "RECORD_COMPLETE",
+                "defaultContent": ""
+            },
+            {
+                "data": "LAST_USER",
+                "defaultContent": "",
+            },
+            {
+                "data": "CREATEDON",
+                "defaultContent": ""
+            },
+            {
+                "data": "UPDATEDON",
+                "defaultContent": ""
+            }
+        ]
+    };
+    this.editor = {
+        idSrc: "ID",
+        table: this.tableId,
+        fields: [{
+                "name": "UPC",
+                "label": "UPC"
+            },
+            {
+                "name": "TRACKED_UPC",
+                "label": "TRACKED_UPC"
+            },
+            {
+                "name": "DESCRIPTION",
+                "label": "Description"
+            },
+            {
+                "name": "IN_SJC",
+                "label": "In_SJC"
+            },
+            {
+                "name": "OUT_SJC",
+                "label": "Out_SJC"
+            },
+            {
+                "name": "PHOTODONE",
+                "label": "Photo Done"
+            },
+            {
+                "name": "RECORD_COMPLETE",
+                "label": "Complete"
+            },
+        ]
+    };
 }
 model_offers.prototype = Object.create(model_base.prototype);
 if(typeof this.MODELS == "undefined"){
